9f96330417fbcf59cd0585b10bce12b4880fdcf9
zhanhui913 <zhanhui913@gmail.com>
2017-08-04 17:04:48 -0700

    weird bug again

--- src/com/example/app2/MainActivity.java
+++ src/com/example/app2/MainActivity.java
@@ -40,6 +40,6 @@ public class MainActivity extends CordovaActivity
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
         
-        Log.d("TEST", "App flavours increased for submodule hooksssss”);
+        Log.d("TEST", "App flavours increased for submodule hooksssss");
     }
 }
